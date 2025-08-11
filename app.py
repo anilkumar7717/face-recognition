9ab9aaf83c89c8af8a178cc31a6f8d82464dfc58
Anil Kumar <anilkumar7717@gmail.com>
2025-08-11 19:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -663,4 +663,4 @@ def health_check():
     return jsonify({'status': 'healthy', 'service': 'face-recognition-api-insightface-enhanced-lowlight'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=8080)
